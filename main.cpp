e8cd6b726cdafd89644a3466987f2208acaee80a
Aditya Patra <adityabpatra@gmail.com>
2025-11-02 13:13:32 -0800

    brainstorm methods

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@
 #include "Goat.h"
 using namespace std;
 
-const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE=25;
+/*
+Additional functions: 
+check avg age of goats - accumulate()
+remove old goats - find + erase
+sell herd - clear
+paint goats - fill
+walk goats - for_each
+sort goats - sort
+shuffle herd - shuffle
+find goats of certain color - find
+*/
+
+const int SZ_NAMES = 200, SZ_COLORS = 25;
 
 int select_goat(list<Goat> trip);
 void delete_goat(list<Goat> &trip);
